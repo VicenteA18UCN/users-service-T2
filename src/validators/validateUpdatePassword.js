cd3bca24c9274a183ec01d680066762cf306f897
VicenteA18UCN <vicente.alarcon@alumnos.ucn.cl>
2024-12-15 20:14:58 -0300

    fix: change validators in update passoword and profile

--- src/validators/validateUpdatePassword.js
+++ src/validators/validateUpdatePassword.js
@@ -8,35 +8,17 @@ export const updatePasswordSchema = Joi.object({
       "string.guid": "El ID del usuario debe ser un UUID válido",
       "any.required": "El ID del usuario es requerido",
     }),
-  oldPassword: Joi.string()
-    .min(10)
-    .max(16)
-    .pattern(/^(?=.*[a-zA-Z])(?=.*\d).+$/)
-    .required()
-    .messages({
-      "string.pattern.base":
-        "La nueva contraseña debe contener al menos una letra y un número",
-      "string.min": "La contraseña actual debe tener al menos 6 caracteres",
-      "string.max": "La contraseña actual no puede exceder los 16 caracteres",
-      "any.required": "La contraseña actual es requerida",
-    }),
-  newPassword: Joi.string()
-    .min(10)
-    .max(16)
-    .required()
-    .pattern(/^(?=.*[a-zA-Z])(?=.*\d).+$/)
-    .messages({
-      "string.pattern.base":
-        "La nueva contraseña debe contener al menos una letra y un número",
-      "string.min": "La nueva contraseña debe tener al menos 6 caracteres",
-      "string.max": "La nueva contraseña no puede exceder los 16 caracteres",
-      "any.required": "La nueva contraseña es requerida",
-    }),
+  oldPassword: Joi.string().required().messages({
+    "any.required": "La contraseña actual es requerida",
+  }),
+  newPassword: Joi.string().required().messages({
+    "any.required": "La nueva contraseña es requerida",
+  }),
   confirmNewPassword: Joi.string()
     .valid(Joi.ref("newPassword"))
     .required()
     .messages({
       "any.only": "Las contraseñas no coinciden",
-      "any.required": "Es obligatorio repetir la contraseña"
+      "any.required": "Es obligatorio repetir la contraseña",
     }),
 });
--- src/validators/validateUpdateProfile.js
+++ src/validators/validateUpdateProfile.js
@@ -1,20 +1,17 @@
 import Joi from "joi";
 
 export const updateProfileSchema = Joi.object({
-  name: Joi.string().min(3).max(50).required().messages({
-    "string.empty": "El nombre es obligatorio.",
+  name: Joi.string().min(3).max(50).optional().messages({
     "string.min": "El nombre debe tener al menos 3 caracteres.",
     "string.max": "El nombre no debe exceder los 50 caracteres.",
   }),
 
-  firstLastName: Joi.string().min(3).max(30).required().messages({
-    "string.empty": "El primer apellido es obligatorio.",
+  firstLastName: Joi.string().min(3).max(30).optional().messages({
     "string.min": "El primer apellido debe tener al menos 3 caracteres.",
     "string.max": "El primer apellido no debe exceder los 30 caracteres.",
   }),
 
-  secondLastName: Joi.string().min(3).max(30).required().messages({
-    "string.empty": "El segundo apellido es obligatorio.",
+  secondLastName: Joi.string().min(3).max(30).optional().messages({
     "string.min": "El segundo apellido debe tener al menos 3 caracteres.",
     "string.max": "El segundo apellido no debe exceder los 30 caracteres.",
   }),
